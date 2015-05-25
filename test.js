eded6a02111ee64361dbc08ce494529efa25c81e
leon.li <leon.li@qunar.com>
2015-05-25 13:21:26 +0800

    change dns way

--- test.js
+++ test.js
@@ -1,9 +1,17 @@
-var http = require("http");
+var exec = require("child_process").exec;
+exec("nslookup qunarzz.com", {"encoding": 'utf-8'},function (err, stdout){
+    var stdout = stdout.split('\r\n');
+    if (stdout[4] && /Address|Addresses/.test(stdout[4])) {
+        var len =  stdout.length;
+        var resultIP;
+        for (var i = 4; i < len; i++) {
 
-http.createServer(function(req, res) {
-    console.log("start");
-    setTimeout(function() {
-        console.log("end");
-        res.end();
-    }, 50000)
-}).listen(3000);
+            if ( /\d+\.\d+\.\d+\.\d+/.test(stdout[i]) ) {
+                resultIP =  /\d+\.\d+\.\d+\.\d+/.exec(stdout[i])[0];
+                console.log(resultIP)
+                i = len;
+            }
+
+        }
+    }
+})
